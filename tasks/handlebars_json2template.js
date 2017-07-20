419f60e1d9d1f2f4b2b5a6dff56714097a1729ae
Dmitry Dudin <nedudi@gmail.com>
2017-07-20 19:13:58 +0300

    Update handlebars_json2template.js

--- tasks/handlebars_json2template.js
+++ tasks/handlebars_json2template.js
@@ -8,65 +8,66 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   var Handlebars = require("handlebars");
 
-  grunt.registerMultiTask('handlebars_json2template', 'The best Grunt plugin ever.', function() {
-    var config = this.data,
-      arrTemplates = [],
-      langTemplates = [],
-      lang = [];
+  grunt.registerMultiTask('handlebars_json2template', 'The best Grunt plugin ever.', function () {
 
-    this.files.forEach(function(f) {
+    console.log(this.data.version);
+
+    var config = this.data, arrTemplates = [], langTemplates = [], lang = [], currentBuildVersion = this.data.version;
+
+    this.files.forEach(function (f) {
       if (f.dest === 'template') {
-        f.src.forEach(function(filepath){
+        f.src.forEach(function (filepath) {
           var nameDir = filepath.split("/").pop(),
             tempArrLang = {};
 
-          grunt.file.recurse(filepath, function(abspath, rootdir, subdir, filename){
+          grunt.file.recurse(filepath, function (abspath, rootdir, subdir, filename) {
             var temp = filename.split(".");
             if (temp[1] === "json") {
-              var isNewLang = lang.some(function(item){
+              var isNewLang = lang.some(function (item) {
                 return item === temp[0];
               });
-              if(!isNewLang) lang.push(temp[0]);
+              if (!isNewLang) lang.push(temp[0]);
               tempArrLang[temp[0]] = abspath;
             }
             if (temp[1] === "html") {
               arrTemplates.push({
                 nameTmp: nameDir,
                 src: abspath
-              })
+              });
             }
           });
           langTemplates.push({
             nameTemplate: nameDir,
             src: tempArrLang
-          })
+          });
         });
       }
     });
 
-    lang.forEach(function(lng){
+    lang.forEach(function (lng) {
       grunt.file.mkdir(config.path + lng);
 
-      arrTemplates.forEach(function(tmpl){
+      arrTemplates.forEach(function (tmpl) {
         var template = Handlebars.compile(grunt.file.read(tmpl.src)),
           data = {},
           nameFile = tmpl["src"].split("/").pop();
 
-        langTemplates.forEach(function(currTempLang){
-          if(currTempLang.nameTemplate === tmpl.nameTmp && currTempLang['src'][lng]){
+        langTemplates.forEach(function (currTempLang) {
+          if (currTempLang.nameTemplate === tmpl.nameTmp && currTempLang['src'][lng]) {
             data = grunt.file.readJSON(currTempLang['src'][lng]);
-
-            if(tmpl.nameTmp === "index") {
-              grunt.file.write(config.path + lng +"/"+ nameFile, template(data));
-            }else {
-              grunt.file.write(config.path + lng + "/"+ tmpl.nameTmp +"/"+ nameFile, template(data));
+            data.CURRENT_BUILD_VERSION = currentBuildVersion;
+            var compiled = template(data);
+            if (tmpl.nameTmp === "index") {
+              grunt.file.write(config.path + lng + "/" + nameFile, compiled);
+            } else {
+              grunt.file.write(config.path + lng + "/" + tmpl.nameTmp + "/" + nameFile, compiled);
             }
           }
         });
-      })
+      });
     });
   });
 };
